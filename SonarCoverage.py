dd9aa67f533f125e1498d22f0943ab0f8b20e616
pktrigg <p.kennedy@fugro.com>
2016-06-07 19:03:36 +0800

    initial re-commit

---
+++ SonarCoverage.py
@@ -0,0 +1,261 @@
+#name:          SonarCoverage.py
+#created:       May 2016
+#by:            p.kennedy@fugro.com
+#description:   This script will compute a nadir gap outline around a trackplot from the Deeptow sonar in an efficient manner
+# It will use an algorithm as agreed between Fugro and ATSB on the definition of the altitude/gap region
+#notes:         See main at end of script for example how to use this
+#based on XTF version 26 18/12/2008
+#version 1.00
+
+#DONE
+# added -odix to control the output folder
+# create a WGS84 prj file.  Good for ArcMap, but not always correct if we have an east/north XTF FileExistsError
+# added support for xtf wildcards
+# added support for splitting polygons based on minimum gap width
+# added support for -o output file name, or default, which is the first input file to be processed.  This means for processing 1 file you do not need to specify the output.
+# This script will compute a nadir gap outline around a trackplot from the Deeptow sonar in an efficient manner
+# designed to handle both geographic and grid positions seamlessly
+# creates a shapefile of points representing nadir sensor position
+# creates a coverage polygon representing the slant range setting of the sonar
+# creates a nadir gap polygon representing the null area under the sidescan which is poor quality data.
+# uses the sensor position, altitude and heading rather than computing CMG heading which is a bit wobbly when navigation data has duplicate points
+# initial implementation
+
+import math
+import argparse
+import sys
+import shapefile
+import csv
+import pyXTF
+import geodetic
+import os
+from glob import glob
+# from pyproj import Proj, transform
+import time
+
+def calcGap(altitude):
+    return (altitude * 0.33) + 12
+    
+def isValidGap(altitude, gap):
+    MINIMUMGAP = 25
+    if gap < MINIMUMGAP:
+        return False
+    return True
+
+# from: http://mathforum.org/library/drmath/view/62034.html
+def calculateRangeBearingFromPosition(easting1, northing1, easting2, northing2):
+    """given 2 east, north, pairs, compute the range and bearing"""
+
+    dx = easting2-easting1
+    dy = northing2-northing1
+
+    bearing = 90 - (180/math.pi)*math.atan2(northing2-northing1, easting2-easting1)
+    return (math.sqrt((dx*dx)+(dy*dy)), bearing)
+
+
+# taken frm http://gis.stackexchange.com/questions/76077/how-to-create-points-based-on-the-distance-and-bearing-from-a-survey-point
+def calculatePositionFromRangeBearing(easting, northing, distance, bearing):
+    """given an east, north, range and bearing, compute a new coordinate on the grid"""
+    point =   (easting, northing)
+    angle =   90 - bearing
+    bearing = math.radians(bearing)
+    angle =   math.radians(angle)
+
+    # polar coordinates
+    dist_x = distance * math.cos(angle)
+    dist_y = distance * math.sin(angle)
+
+    xfinal = point[0] + dist_x
+    yfinal = point[1] + dist_y
+
+    # direction cosines
+    cosa = math.cos(angle)
+    cosb = math.cos(bearing)
+    xfinal = point[0] + (distance * cosa)
+    yfinal = point[1] + (distance * cosb)
+    
+    return [xfinal, yfinal]
+
+def main():
+
+    start_time = time.time() # time the process
+    parser = argparse.ArgumentParser(description='Read XTF file and create either a coverage or Nadir gap polygon.')
+    parser.add_argument('-c', action='store_true', default=False, dest='createCoveragePolygon', help='-c compute a polygon across the entire sonar region, ie COVERAGE')
+    parser.add_argument('-n', action='store_true', default=False, dest='createNadirPolygon', help='-n compute a polygon across the NADIR region')
+    parser.add_argument('-i', dest='inputFile', action='store', help='-i <filename> input filename in ASCI Easting,Northing,Altidude comma separated format')
+    parser.add_argument('-o', dest='outputFile', action='store', help='-o <filename> output shape filename. Do not provide file extension. It will be added for you  [default = Nadir_pg]')
+    parser.add_argument('-odix', dest='outputFolder', action='store', help='-odix <folder> output folder to store shape files.  If not specified, the files will be alongside the input XTF file')
+    
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
+    
+    args = parser.parse_args()
+   
+    if args.outputFolder == None:
+        firstFile = glob(args.inputFile)[0]
+        args.outputFolder = os.path.abspath(os.path.join(firstFile, os.pardir))
+
+    shp_pt = shapefile.Writer(shapefile.POINT)
+    # for every record there must be a corresponding geometry.
+    shp_pt.autoBalance = 1
+    shp_pt.field('XTFFile', 'C', 255)
+    
+    shp_pg = shapefile.Writer(shapefile.POLYGON)
+    shp_pg.autBalance = 1 #ensures gemoetry and attributes match
+    shp_pg.field('XTFFile', 'C', 255)
+    # shp_pg.field('ALTITUDE_FLD')
+
+    for filename in glob(args.inputFile):
+        if args.createNadirPolygon:       
+            computeNadir(filename, shp_pt, shp_pg)
+        else:
+            print ("option not yet implemented!.  Try '-n' to compute nadir gaps")
+            exit (0)
+
+    if args.outputFile is None:
+        baseName = os.path.basename(os.path.splitext(glob(args.inputFile)[0])[0])
+        pointFile = os.path.join(args.outputFolder, baseName + "_pt")
+        # pointFile = args.outputFolder + baseName + "_pt"
+        polyFile = os.path.join(args.outputFolder, baseName + "_pg")
+        # polyFile = baseName + "_pg"
+    else:
+        pointFile = args.outputFile + "_pt"
+        polyFile = args.outputFile + "_pg"
+
+    print("saving shapefile...")
+    #Save shapefiles
+    shp_pt.save(pointFile)
+    shp_pg.save(polyFile)
+    print("save complete.")
+
+    # now write out the prj file of spatial reference, so we can open in ArcMap
+    prj = open(polyFile + ".prj", "w")
+    prj.write('GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]')
+    # epsg = getWKT_PRJ("4326")
+    # prj.write(epsg)
+    prj.close() 
+    prj = open(pointFile + ".prj", "w")
+    prj.write('GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]')
+    # epsg = getWKT_PRJ("4326")
+    # prj.write(epsg)
+    prj.close() 
+    
+    print("--- %s seconds ---" % (time.time() - start_time)) # print the processing time.
+
+    return (0)
+
+def savePolygon(leftSide, rightSide, shp_pg, shp_pt, fileName):
+    
+    #now build the outline polygon and store to shapefile
+    outline = []
+    print("merging polygon vertices...")
+    for pt in leftSide:
+        outline.append(pt)
+    rightSide.reverse()
+    for pt in rightSide:
+        outline.append(pt)
+        
+    print("creating geometry...")
+        
+    shp_pg.poly(parts=[outline]) #write the geometry
+    shp_pg.record(fileName)              
+    leftSide.clear()
+    rightSide.clear()
+
+def computeNadir(filename, shp_pt, shp_pg):
+
+    leftSide = [] #storage for the left side of the nadir polygon
+    rightSide = [] #storage for the left side of the nadir polygon.  This will be added to teh right side to close the polygon
+    prevEast = 0 
+
+    #   open the trackplot file for reading 
+    print ("Opening file:", filename)
+    r = pyXTF.XTFReader(filename)
+    while r.moreData():
+        pingHdr = r.readPing()
+        if prevEast == 0:
+            prevEast = pingHdr.SensorXcoordinate
+            prevNorth = pingHdr.SensorYcoordinate
+            prevAltitude = pingHdr.SensorPrimaryAltitude
+            continue
+            
+        currEast = pingHdr.SensorXcoordinate
+        currNorth = pingHdr.SensorYcoordinate
+        currAltitude = pingHdr.SensorPrimaryAltitude
+
+        #add ping position to a shape file for QC purposes
+        shp_pt.point(currEast,currNorth)
+        shp_pt.record(filename)
+ 
+        # compute the range based on the user requesting either coverage polygons or nadir gap polygins
+        currRange = calcGap(currAltitude)
+
+        if isValidGap(currAltitude, currRange) == False:
+            if len(leftSide) > 2:
+                savePolygon(leftSide, rightSide, shp_pg, shp_pt, filename)
+            continue
+
+        if (pingHdr.SensorXcoordinate < 180) & (pingHdr.SensorYcoordinate < 90):
+            #compute with geographical data
+            # rng, currBearing, backBearing = geodetic.vinc_dist(prevNorth, prevEast, currNorth, currEast )
+            currBearing = pingHdr.SensorHeading
+            # compute the left side and add to a list
+            leftSideNorthing, leftSideEasting, alpha21 = geodetic.vincentyDirect(currNorth, currEast, currBearing - 90, currRange)
+            leftSide.append([leftSideEasting,leftSideNorthing])
+            # shp_pt.point(leftSideEasting,leftSideNorthing)
+            # shp_pt.record(currAltitude)
+
+            # compute the right side and add to a list 
+            rightSideNorthing, rightSideEasting, alpha21 = geodetic.vincentyDirect(currNorth, currEast, currBearing + 90, currRange)
+            rightSide.append([rightSideEasting,rightSideNorthing])
+            # shp_pt.point(rightSideEasting,rightSideNorthing)
+            # shp_pt.record(currAltitude)  
+        else:
+            # compute with grid data
+            # calculate the heading instead of using the gyro field. it is not always present! 
+            rng, currBearing = calculateRangeBearingFromPosition(prevEast, prevNorth, currEast, currNorth)
+            currBearing = pingHdr.SensorHeading
+            # compute the left side and add to a list
+            leftSideEasting, leftSideNorthing = calculatePositionFromRangeBearing(currEast, currNorth, currRange, currBearing - 90.0)
+            leftSide.append([leftSideEasting,leftSideNorthing])
+            # shp_pt.point(leftSideEasting,leftSideNorthing)
+            # shp_pt.record(currAltitude)
+            
+            # compute the right side and add to a list
+            rightSideEasting, rightSideNorthing = calculatePositionFromRangeBearing(currEast, currNorth, currRange, currBearing + 90.0)
+            rightSide.append([rightSideEasting,rightSideNorthing])
+            # shp_pt.point(rightSideEasting,rightSideNorthing)
+            # shp_pt.record(currAltitude)  
+        
+        prevEast = currEast
+        prevNorth = currNorth
+        prevAltitude = currAltitude
+
+        if pingHdr.PingNumber % 500 == 0:
+            print ("Ping: %f, X: %f, Y: %f, A: %f Range: %f Bearing %f" % (pingHdr.PingNumber, currEast, currNorth, currAltitude, currRange, currBearing))               
+    
+    print("Complete reading XTF file :-)")
+
+    savePolygon(leftSide, rightSide, shp_pg, shp_pt, filename)
+
+    # w.poly(parts=[[[1,3],[5,3]]], shapeType=shapefile.POLYLINE)
+    # w.field('FIRST_FLD','C','40')
+    # w.field('SECOND_FLD','C','40')
+    # w.record('First','Line')
+    # w.record('Second','Line')
+    # w.save('shapefiles/test/line')
+
+def isHeader(row):
+    for word in row:
+        if "#" in word: #skip headers
+            return True
+    return False
+
+if __name__ == "__main__":
+    main()
+
+    # east, north = calculatePositionFromRangeBearing(1000.00, 1000, 10, 0)
+    # calculatePositionFromRangeBearing(1000.00, 1000, 10, 90)
+    # calculatePositionFromRangeBearing(1000.00, 1000, 10, 180)
+    # calculatePositionFromRangeBearing(1000.00, 1000, 10, 270)
---
+++ pyXTF.py
@@ -0,0 +1,286 @@
+#name:          pyXTF
+#created:       May 2016
+#by:            p.kennedy@fugro.com
+#description:   script to read an XTF file
+#notes:         See main at end of script for example how to use this
+#based on XTF version 26 18/12/2008
+#version 1.00
+
+# XTF BYTE = 1 byte = "b"
+# XTFWORD = signed int 2 bytes = h
+# DWORD = unsigned int 4 bytes = "L"
+# short = short integer 2 bytes = "h"
+# char = 1 byte = "c"
+
+#DONE
+#initial implementation
+
+import pprint
+import struct
+import os.path
+
+class XTFPINGHEADER:
+    def __init__(self, fileptr):
+        XTFPingHeader_fmt = '=h2b3hLh6bh2L2fL21f2d2h 4b2f2d4h10flfl4b2hB11b'
+        XTFPingHeader_len = struct.calcsize(XTFPingHeader_fmt)
+        XTFPingHeader_unpack = struct.Struct(XTFPingHeader_fmt).unpack_from
+
+        data = fileptr.read(XTFPingHeader_len)
+        s = XTFPingHeader_unpack(data)
+
+        self.MagicNumber                    = s[0]
+        self.HeaderType                     = s[1]
+        self.SubChannelNumber               = s[2]
+        self.NumChansToFollow               = s[3]
+        self.Reserved1                      = s[4]
+        self.Reserved2                      = s[5]
+        self.NumBytesThisRecord             = s[6]
+        self.Year                           = s[7]
+        self.Month                          = s[8]
+        self.Day                            = s[9]
+        self.Hour                           = s[10]
+        self.Minute                         = s[11]
+        self.Second                         = s[12]
+        self.HSeconds                       = s[13]
+        self.JulianDays                     = s[14]
+        self.EventNumber                    = s[15]
+        self.PingNumber                     = s[16]
+        self.SoundVelocity                  = s[17]
+        self.OceanTide                      = s[18]
+        self.Reserved2                      = s[19]
+        self.ConductivityFreq               = s[20]
+        self.TemperatureFreq                = s[21]
+        self.PressureFreq                   = s[22]
+        self.PressureTemp                   = s[23]
+        self.Conductivity                   = s[24]
+        self.WaterTemperature               = s[25]
+        self.Pressure                       = s[26]
+        self.ComputedSoundVelocity          = s[27]
+        self.MagX                           = s[28]
+        self.MagY                           = s[29]
+        self.MagZ                           = s[30]
+        self.AuxVal1                        = s[31]
+        self.AuxVal2                        = s[32]
+        self.AuxVal3                        = s[33]
+        self.AuxVal4                        = s[34]
+        self.AuxVal5                        = s[35]
+        self.AuxVal6                        = s[36]
+        self.SpeedLog                       = s[37]
+        self.Turbidity                      = s[38]
+        self.ShipSpeed                      = s[39]
+        self.ShipGyro                       = s[40]
+        self.ShipYcoordinate                = s[41]                        
+        self.ShipXcoordinate                = s[42]
+        self.ShipAltitiude                  = s[43]
+        self.ShipDepth                      = s[44]
+        self.FixTimeHour                    = s[45]
+        self.FixTimeMinute                  = s[46]
+        self.FixTimeSecond                  = s[47]
+        self.FixTimeHsecond                 = s[48]
+        self.SensorSpeed                    = s[49]
+        self.KP                             = s[50]
+        self.SensorYcoordinate              = s[51]
+        self.SensorXcoordinate              = s[52]
+        self.SonarStatus                    = s[53]
+        self.RangeToTowFish                 = s[54]
+        self.BearingToTowFish               = s[55]
+        self.CableOut                       = s[56]
+        self.Layback                        = s[57]
+        self.CableTension                   = s[58]
+        self.SensorDepth                    = s[59]
+        self.SensorPrimaryAltitude          = s[60]
+        self.SensorAuxAltitude              = s[61]
+        self.SensorPitch                    = s[62]
+        self.SensorRoll                     = s[63]
+        self.SensorHeading                  = s[64]
+        self.Heave                          = s[65]
+        self.Yaw                            = s[66]
+        self.AttitudeTimeTag                = s[67]
+        self.DOT                            = s[68]
+        self.NavFixMilliseconds             = s[69]
+        self.ComputerClockHour              = s[70]
+        self.ComputerClockMinute            = s[71]
+        self.ComputerClockSecond            = s[72]
+        self.ComputerClockHSecond           = s[73]
+        self.FishPositionDeltaX             = s[74]
+        self.FishPositionDeltaY             = s[75]
+        self.FishPositionErrorCode          = s[76]
+        self.ReservedSpace2                 = s[77]
+
+        # now read the chaninfo records.  This is more complex than it needs to be, but for now, read six channels
+        self.pingChannel =[]
+        for i in range(self.NumChansToFollow):
+            ping = XTFPINGCHANHEADER(fileptr)
+            self.pingChannel.append(ping)
+
+    def __str__(self):
+        return (pprint.pformat(vars(self)))        
+                
+class XTFPINGCHANHEADER:
+    def __init__(self, fileptr):
+        XTFPingChanHeader_fmt = '=2h5f5hLh2bLhf2bfh4b'
+        XTFPingChanHeader_len = struct.calcsize(XTFPingChanHeader_fmt)
+        XTFPingChanHeader_unpack = struct.Struct(XTFPingChanHeader_fmt).unpack_from
+
+        hdr = fileptr.read(XTFPingChanHeader_len)
+        s = XTFPingChanHeader_unpack(hdr)
+        self.ChannelNumber                    = s[0]
+        self.DownsampleMethod                 = s[1]
+        self.SlantRange                       = s[2]
+        self.GroundRange                      = s[3]
+        self.TimeDelay                        = s[4]
+        self.TimeDuration                     = s[5]
+        self.SecondsPerPing                   = s[6]
+        self.ProcessingFlags                  = s[7]
+        self.Frequency                        = s[8]
+        self.InitialGainCode                  = s[9]
+        self.GainCode                         = s[10]
+        self.BandWidth                        = s[11]
+        self.ContactNumber                    = s[12]
+        self.ContactClassification            = s[13]
+        self.ContactSubNumber                 = s[14]
+        self.ContactType                      = s[15]
+        self.NumSamples                       = s[16]
+        self.MillivoltScale                   = s[17]
+        self.ContactTimeOffTrack              = s[18]
+        self.ContactCloseNumber               = s[19]
+        self.Reserved2                        = s[20]
+        self.FixedVSOP                        = s[21]
+        self.Weight                           = s[22]
+        self.ReservedSpace1                   = s[23]
+        self.ReservedSpace2                   = s[24]
+        self.ReservedSpace3                   = s[25]
+        self.ReservedSpace4                   = s[26]
+        
+        #now read the sonar data
+        XTFdata_fmt = '=' + str(self.NumSamples) + 'h'
+        XTFdata_len = struct.calcsize(XTFdata_fmt)
+        XTFdata_unpack = struct.Struct(XTFdata_fmt).unpack_from
+        self.data = fileptr.read(XTFdata_len)
+
+        return
+        
+    def __str__(self):
+        return (pprint.pformat(vars(self)))        
+        
+class XTFCHANINFO:
+    def __init__(self, fileptr):
+        XTFChanInfo_fmt = '=bb3hl16s11fhb53s'
+        XTFChanInfo_len = struct.calcsize(XTFChanInfo_fmt)
+        XTFChanInfo_unpack = struct.Struct(XTFChanInfo_fmt).unpack_from
+
+        data = fileptr.read(XTFChanInfo_len)
+        s = XTFChanInfo_unpack(data)
+        self.TypeOfChannel                    = s[0]
+        self.SubChannelNumber                 = s[1]
+        self.CorrectionFlags                  = s[2]
+        self.UniPolar                         = s[3]
+        self.BytesPerSample                   = s[4]
+        self.Reserved                         = s[5]
+        self.ChannelName                      = s[6].decode('utf-8').rstrip('\x00')
+        self.VoltScale                        = s[7]
+        self.Frequency                        = s[8]
+        self.HorizBeamAngle                   = s[9]
+        self.TiltAngle                        = s[10]
+        self.BeamWidth                        = s[11]
+        self.OffsetX                          = s[12]
+        self.OffsetY                          = s[13]
+        self.OffsetZ                          = s[14]
+        self.OffsetYaw                        = s[15]
+        self.OffsetPitch                      = s[16]
+        self.OffsetRoll                       = s[17]
+        self.BeamsPerArray                    = s[18]
+        self.SampleFormat                     = s[19]
+        self.ReservedArea2                    = s[20].decode('utf-8').rstrip('\x00')
+                        
+    def __str__(self):
+        return (pprint.pformat(vars(self)))
+
+class XTFFILEHDR:
+    def __init__(self, fileptr):
+        XTFFileHdr_fmt = '=bb8s8s16sh64s64s3hbbhbbHf12b10bl12f'
+        XTFFileHdr_len = struct.calcsize(XTFFileHdr_fmt)
+        XTFFileHdr_unpack = struct.Struct(XTFFileHdr_fmt).unpack_from
+
+        data = fileptr.read(XTFFileHdr_len)
+        s = XTFFileHdr_unpack(data)
+        self.FileFormat                         = s[0]
+        self.SystemType                         = s[1]
+        self.RecordingProgramName               = s[2].decode('utf-8').rstrip('\x00')
+        self.RecordingProgramVersion            = s[3].decode('utf-8').rstrip('\x00')
+        self.SonarName                          = s[4].decode('utf-8').rstrip('\x00')
+        self.SonarType                          = s[5]
+        self.NoteString                         = s[6].decode('utf-8').rstrip('\x00')
+        self.ThisFileName                       = s[7].decode('utf-8').rstrip('\x00')
+        self.NavUnits                           = s[8]
+        self.NumberOfSonarChannels              = s[9]
+        self.NumberOfBathymetryChannels         = s[10]
+        self.NumberOfSnippetChannels            = s[11]
+        self.NumberOfForwardLookArrays          = s[12]
+        self.NumberOfInterferometryChannels     = s[13]
+        self.Reserved1                          = s[14]
+        self.Reserved2                          = s[15]
+        self.ReferencePointHeight               = s[16]
+        self.ProjectionType                     = s[17]
+        self.SpheroidType                       = s[18]
+        self.NavigationLatency                  = s[19]
+        self.OriginX                            = s[20]
+        self.Originy                            = s[21]
+        self.NavoffsetX                         = s[22]
+        self.NavoffsetY                         = s[23]
+        self.NavoffsetZ                         = s[24]
+        self.NavoffsetYaw                       = s[25]
+        self.NavoffsetX                         = s[26]
+        self.MRUoffsetY                         = s[27]
+        self.MRUoffsetZ                         = s[28]
+        self.MRUoffsetYaw                       = s[29]
+        self.MRUoffsetPitch                     = s[30]
+        self.MRUoffsetRoll                      = s[31]
+
+        # now read the chaninfo records.  This is more complex than it needs to be, but for now, read six channels
+        self.XTFChanInfo =[]
+        for i in range(6):
+            ch = XTFCHANINFO(fileptr)
+            self.XTFChanInfo.append(ch)
+            
+    def __str__(self):
+        return (pprint.pformat(vars(self)))
+    
+class XTFReader:
+    def __init__(self, XTFfileName):
+        if not os.path.isfile(XTFfileName):
+            print ("file not found:", XTFfileName)
+        self.fileName = XTFfileName
+        self.fileptr = open(XTFfileName, 'rb')        
+        self.fileSize = self.fileptr.seek(0, 2)
+        # go back to start of file
+        self.fileptr.seek(0, 0)
+                
+        self.XTFFileHdr = XTFFILEHDR(self.fileptr)
+            
+    def __str__(self):
+        return pprint.pformat(vars(self))
+        
+    def moreData(self):
+        bytesRemaining = self.fileSize - self.fileptr.tell()
+        return bytesRemaining
+                
+    def readPing(self):
+        ping = XTFPINGHEADER(self.fileptr)
+        return ping
+    
+    def readChannel(self):        
+        return XTFPINGCHANHEADER(self.fileptr)
+         
+if __name__ == "__main__":
+    r = XTFReader("C:/development/python/SonarNadirCalculator/01064_m66c448_SSS_20151219_205405_HH_HuginES7_GA4450_P_compressed.xtf")
+    print (r)
+    print (r.XTFFileHdr)
+    for ch in range(r.XTFFileHdr.NumberOfSonarChannels):
+        print(r.XTFFileHdr.XTFChanInfo[ch])
+
+    while r.moreData():
+        pingHdr = r.readPing()
+        print (pingHdr.PingNumber,  pingHdr.SensorXcoordinate, pingHdr.SensorYcoordinate)
+            
+    print("Complete reading XTF file :-)")
